11b526dd3794950f8e6877c52927c0479a0627a7
SwiftCodes01 <83612531+SwiftCodes01@users.noreply.github.com>
2025-10-16 14:27:16 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import Hambugger from "../assets/Group 1.png";
+// import Hambugger from "../assets/Group 1.png";
 import MobileNavMenu from "./MobileNavMenu";
 
 export default function Navbar() {
